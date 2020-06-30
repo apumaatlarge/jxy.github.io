42efbf036ac56b6167b8b2a18c1499d79b3acaf8
apumaatlarge <49434108+apumaatlarge@users.noreply.github.com>
2020-06-30 09:53:38 +0800

    Add files via upload

---
+++ test-site/scripts/main.js
@@ -0,0 +1,26 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src', 'images/lxt.jpg');
+    } else {
+      myImage.setAttribute('src', 'images/lxt2.jpg');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = '来晓彤的老公，' + myName;
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = '来晓彤的老公，' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+ }
